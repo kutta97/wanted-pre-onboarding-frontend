9a14fa9f329b03f5a858f80c89051b44313e794a
kutta97 <yhjin04170@gmail.com>
2023-04-19 02:36:20 +0900

    refactor: Signup 페이지에서 Inputs 컴포넌트 사용하도록 리팩토링

--- src/pages/singup/hooks/useSignup.js
+++ src/pages/singup/hooks/useSignup.js
@@ -1,26 +1,4 @@
-import { useEffect, useState } from 'react';
-import { validateEmail, validatePassword } from '../../../utils/validation';
-
 const useSignup = () => {
-  const [inputs, setInputs] = useState({
-    email: '',
-    password: '',
-  });
-  const [isFormValid, setIsFormValid] = useState(false);
-
-  useEffect(() => {
-    const { email, password } = inputs;
-    setIsFormValid(validateEmail(email) && validatePassword(password));
-  }, [inputs]);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
-
   const handleSignup = (e) => {
     e.preventDefault();
     const [email, password] = e.target.elements;
@@ -28,8 +6,6 @@ const useSignup = () => {
   };
 
   return {
-    isFormValid,
-    handleChange,
     handleSignup,
   };
 };
