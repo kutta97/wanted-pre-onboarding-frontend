88e62493943577d53ac4b1170f4ed339d0641d72
kutta97 <yhjin04170@gmail.com>
2023-04-18 23:47:25 +0900

    feat: routing 구현 ('/singup', '/signin', '/todo')

---
+++ src/pages/signin/Singin.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Signin = () => (
+  <div>
+    <p>signin</p>
+  </div>
+);
+
+export default Signin;
---
+++ src/pages/todo/Todo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Todo = () => (
+  <div>
+    <p>todo</p>
+  </div>
+);
+
+export default Todo;
