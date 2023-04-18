29e803f6596505981a9b2a3a205c6c2d4da275b1
kutta97 <yhjin04170@gmail.com>
2023-04-19 02:32:51 +0900

    feat: 로그인, 회원가입 페이지 공통 Inputs 컴포넌트 개발

---
+++ src/components/inputs/hooks/useInputs.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { validateEmail, validatePassword } from '../../../utils/validation';
+
+const useInputs = () => {
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+  });
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(() => {
+    const { email, password } = inputs;
+    setIsFormValid(validateEmail(email) && validatePassword(password));
+  }, [inputs]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  return {
+    isFormValid,
+    handleChange,
+  };
+};
+
+export default useInputs;
