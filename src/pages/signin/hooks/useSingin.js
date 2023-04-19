40ed73dc6707fa127fa22a8f03254b435c723ce1
kutta97 <yhjin04170@gmail.com>
2023-04-19 15:54:19 +0900

    feat: 로그인 페이지와 signin api 연결 및, 로그인 성공 시 todo 페이지 리다이렉트

--- src/pages/signin/hooks/useSingin.js
+++ src/pages/signin/hooks/useSingin.js
@@ -1,8 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+import { signin } from '../../../api/signin/signin';
+import { JWT_TOKEN } from '../../../consts/localStorage';
+import { setStorage } from '../../../utils/storage';
+
 const useSignin = () => {
-  const handleSignin = (e) => {
+  const navigate = useNavigate();
+
+  const handleSignin = async (e) => {
     e.preventDefault();
     const [email, password] = e.target.elements;
-    console.log(email.value, password.value);
+    const userInfo = {
+      email: email.value,
+      password: password.value,
+    };
+    const { accesToken } = await signin(userInfo);
+
+    if (accesToken !== null) {
+      setStorage(JWT_TOKEN, accesToken);
+      return navigate('/todo');
+    }
+    return null;
   };
 
   return {
