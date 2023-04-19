e2cd571d924e1f030af29dd72ec4b963b4aac765
kutta97 <yhjin04170@gmail.com>
2023-04-20 07:00:02 +0900

    refactor: Inputs 컴포넌트 이름 AccountInputs 로 변경

---
+++ src/components/accountInputs/AccountInputs.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { getUserData } from '@utils/formData';
+
+import useAccountInputs from './hooks/useAccountInputs';
+
+const AccountInputs = (props) => {
+  const { buttonTestid, onSubmit, children } = props;
+  const { isFormValid, handleChange } = useAccountInputs();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const accountData = getUserData(formData);
+    onSubmit?.(accountData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="email" data-testid="email-input" onChange={handleChange} />
+      <input
+        name="password"
+        data-testid="password-input"
+        onChange={handleChange}
+      />
+      <button data-testid={buttonTestid} type="submit" disabled={!isFormValid}>
+        {children}
+      </button>
+    </form>
+  );
+};
+
+export default AccountInputs;
