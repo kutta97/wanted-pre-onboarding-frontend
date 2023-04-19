77fbdd2af3ba9359fb4d643fc9c73f0ce423e5fd
kutta97 <yhjin04170@gmail.com>
2023-04-20 01:13:27 +0900

    feat: 로그인 여부에 따라 페이지 이동하는 컴포넌트 추가

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+
+const Home = () => {
+  const auth = useAuth();
+
+  if (!auth) {
+    return <Navigate to="/signin" replace />;
+  }
+  return <Navigate to="/todo" replace />;
+};
+
+export default Home;
